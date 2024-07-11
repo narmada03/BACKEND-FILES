dbf6f16684e818f34ded6975bc508233a944fe13
Narmada Gandla <narmadagandla@gmail.com>
2024-07-11 22:11:38 +0530

    Add files via upload

---
+++ 10-07-24/Demo_03.java
@@ -0,0 +1,28 @@
+public class Demo_03{
+	public static void main(String[]args){
+		int j=2030290035;
+		byte b=50;
+		short s=250;
+		int i=9147483647;
+		long l=9030290035L;
+		float f=3.24f;
+		double d=33.34343232;
+		boolean b1=true;
+		boolean b2=false;
+		char c ='r';
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(c);
+		
+	
+	}
+}
